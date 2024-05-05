17e230355047ccb8ef955cfec660730667179f3f
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-05-05 10:08:18 +0530

    Time: 3 ms (64.89%) | Memory: 11.3 MB (51.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.cpp
+++ 162-find-peak-element/find-peak-element.cpp
@@ -10,21 +10,34 @@ public:
         //     }
         // }
         // return -1;
+          
+          if(n==1) return 0;
+          if(nums[0] > nums[1]) return 0;
+          if(nums[n-1] > nums[n-2]) return n-1;
+          int low=1;
+          int high = n-2;
 
-        int s=0;
-        int e=n-1;
-        while(s<e)
-        {
-            int mid= s+(e-s)/2;
-            if(nums[mid]<nums[mid+1])
-            {
-                s=mid+1;
-            }
-            else
-            {
-                e=mid;
-            }
-        }
-        return s;
-    }
+          while(low<= high)
+          {
+
+            int mid = low +(high-low)/2;
+                  if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1])
+                  {
+                    return mid;
+                  }
+
+                  else if(nums[mid] > nums[mid-1])
+                  {
+                    low=mid+1;
+                  }
+                  else 
+                  {
+                    high = mid-1;
+                  }
+
+          }
+                  return -1;
+
+          }
+           
 };
