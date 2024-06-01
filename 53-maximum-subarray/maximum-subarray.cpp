338953a9dc28365d09c3ac55983d4c92dbf43dc7
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-06-01 13:44:15 +0530

    Time: 146 ms (5.23%) | Memory: 70 MB (92.70%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,22 +1,54 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int n=nums.size();
-        int maxi= INT_MIN;
-        int sum=0;
-        for(int i=0;i<n;i++)
-        {
-             sum=sum+ nums[i];
-             if(sum > maxi)
-             {
-                maxi=sum;
-             }
-             if(sum <0)
-             {
-                sum=0;
-             }
-        }
-        return maxi;
+        // int n=nums.size();
+        // int maxi= INT_MIN;
+        // int sum=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //      sum=sum+ nums[i];
+        //      if(sum > maxi)
+        //      {
+        //         maxi=sum;
+        //      }
+        //      if(sum <0)
+        //      {
+        //         sum=0;
+        //      }
+        // }
+        // return maxi;
 
+        int n=nums.size();
+        int maxi=INT_MIN;
+       int sum=0;
+    int start = 0;
+    int ansStart = -1, ansEnd = -1;
+    for (int i = 0; i < n; i++) {
+
+        if (sum == 0) start = i; // starting index
+
+        sum += nums[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    //printing the subarray:
+    cout << "The subarray is: [";
+    for (int i = ansStart; i <= ansEnd; i++) {
+        cout << nums[i] << " ";
+    }
+    cout << "]";
+
+    return maxi;
     }
 };
