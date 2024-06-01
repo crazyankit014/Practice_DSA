33d14cfced4f06e19f85077fd93476b920d4b1e2
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-06-01 16:14:22 +0530

    Time: 73 ms (93.18%) | Memory: 95.8 MB (69.01%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi=INT_MIN;
+        int mini=INT_MAX;
+        int n=prices.size();
+        for(int i=0;i<n;i++)
+        {
+
+            mini=min(mini,prices[i]);
+           maxi=max(maxi,prices[i]-mini);
+        }
+        return maxi;
+    }
+};
