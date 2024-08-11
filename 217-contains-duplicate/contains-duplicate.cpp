537345b3ef2343d0dba9cabecebd4ec83eca0642
ANKIT <91412584+crazyankit014@users.noreply.github.com>
2024-08-11 11:47:13 +0530

    Time: 107 ms (27.75%) | Memory: 75.6 MB (10.20%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.cpp
+++ 217-contains-duplicate/contains-duplicate.cpp
@@ -1,18 +1,8 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mpp;
         int n=nums.size();
-        // int count=0;
-        // for(int i=0;i<n;i++)
-        // {
-        //     for(int j=i+1;j<n;j++)
-        //     if(nums[i]==nums[j])
-        //     {
-        //         return true;
-        //     }
-        // }
-        // return false;
-        unordered_map<int,int>mpp;
         for(int i=0;i<n;i++)
         {
             mpp[nums[i]]++;
@@ -20,7 +10,7 @@ public:
 
         for(auto it: mpp)
         {
-            if(it.second >=2)
+            if(it.second >= 2)
             {
                 return true;
             }
